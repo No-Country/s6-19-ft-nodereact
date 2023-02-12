7b29de7a2424afaa5680dfb0f738d2e2c4a6bde7
rony171998 <rony171998@gmail.com>
2023-02-12 14:03:06 -0500

    first commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-//import './App.css'
-import Contactme from './components/Contactme'
+import './index.css'
+import Contactme from './components/contact/Contactme'
+import Planes from './components/plans/Planes'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div>
+      <Planes />
       <Contactme />
     </div>
   )
---
+++ frontend/src/components/plans/Planes.tsx
@@ -0,0 +1,104 @@
+import IconCheck from '../../assets/Vector.png';
+import IconCancel from '../../assets/leaf quad.png';
+
+const Planes = () => {
+    return (
+        <div className='container mx-auto my-10'>
+            <div className="grid grid-cols-4">
+                <div className="mt-16 mx-3">
+                    <div className='flex justify-end mr-10 mb-24'>
+                        <img src="./src/assets/arcticons_verifit.png" alt="" />
+                    </div>
+                    <h1 className='text-5xl text-center font-bold mb-5'>ELEGÍ UN PLAN ACORDE A VOS</h1>
+                    <h1 className='text-xl text-center font-bold mb-5'>+ 24 reviews</h1>
+                    <div className='flex justify-center'>
+                        <img src="./src/assets/puntitos.png" alt="" />
+                    </div>
+                </div>
+                <div className="">
+                    <div className="bg-white rounded-3xl shadow-lg p-8 border border-gray-400 h-full mx-3">
+                        <div className='flex justify-end'>
+                            <button>
+                                <img src={IconCancel} />
+                            </button>
+                        </div>
+                        <h2 className="text-center text-2xl font-medium my-5">Plan - 3 días</h2>
+                        <hr className="w-2/3 mx-auto mt-20 mb-10 border-gray-300" />
+                        <div className="m-5">
+                            <div className='flex my-5' >
+                                <img src={IconCheck} className='h-5 m-1' />
+                                <p>Lorem ipsum dolor.</p>
+                            </div>
+                            <div className='flex my-5' >
+                                <img src={IconCheck} className='h-5 m-1' />
+                                <p>Lorem ipsum dolor.</p>
+                            </div>
+                            <div className='flex my-5' >
+                                <img src={IconCheck} className='h-5 m-1' />
+                                <p>Lorem ipsum dolor.</p>
+                            </div>
+                        </div>
+                        <button className="bg-purple-600 hover:bg-purple-500 text-white font-medium py-2 px-14 rounded-2xl block mx-auto mt-10 drop-shadow-lg"
+                        >Descargar</button>
+                    </div>
+                </div>
+                <div className="">
+                    <div className="bg-white rounded-3xl shadow-lg p-8 border border-gray-400 h-full mx-3">
+                        <div className='flex justify-end'>
+                            <button>
+                                <img src={IconCancel} />
+                            </button>
+                        </div>
+                        <h2 className="text-center text-2xl font-medium my-5">Plan - 3 días</h2>
+                        <hr className="w-2/3 mx-auto mt-20 mb-10 border-gray-300" />
+                        <div className="m-5">
+                            <div className='flex my-5' >
+                                <img src={IconCheck} className='h-5 m-1' />
+                                <p>Lorem ipsum dolor.</p>
+                            </div>
+                            <div className='flex my-5' >
+                                <img src={IconCheck} className='h-5 m-1' />
+                                <p>Lorem ipsum dolor.</p>
+                            </div>
+                            <div className='flex my-5' >
+                                <img src={IconCheck} className='h-5 m-1' />
+                                <p>Lorem ipsum dolor.</p>
+                            </div>
+                        </div>
+                        <button className="bg-purple-600 hover:bg-purple-500 text-white font-medium py-2 px-14 rounded-2xl block mx-auto mt-10 drop-shadow-lg"
+                        >Descargar</button>
+                    </div>
+                </div>
+                <div className="">
+                    <div className="bg-white rounded-3xl shadow-lg p-8 border border-gray-400 h-full mx-3">
+                        <div className='flex justify-end'>
+                            <button>
+                                <img src={IconCancel} />
+                            </button>
+                        </div>
+                        <h2 className="text-center text-2xl font-medium my-5">Plan - 3 días</h2>
+                        <hr className="w-2/3 mx-auto mt-20 mb-10 border-gray-300" />
+                        <div className="m-5">
+                            <div className='flex my-5' >
+                                <img src={IconCheck} className='h-5 m-1' />
+                                <p>Lorem ipsum dolor.</p>
+                            </div>
+                            <div className='flex my-5' >
+                                <img src={IconCheck} className='h-5 m-1' />
+                                <p>Lorem ipsum dolor.</p>
+                            </div>
+                            <div className='flex my-5' >
+                                <img src={IconCheck} className='h-5 m-1' />
+                                <p>Lorem ipsum dolor.</p>
+                            </div>
+                        </div>
+                        <button className="bg-purple-600 hover:bg-purple-500 text-white font-medium py-2 px-14 rounded-2xl block mx-auto mt-10 drop-shadow-lg"
+                        >Descargar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Planes;
