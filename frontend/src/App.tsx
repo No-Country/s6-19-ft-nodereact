549bfe38e554a765014ca5c9803297908a228eb7
AlexanderC <alex88_upt@hotmail.com>
2023-02-12 19:11:15 -0500

    first merge

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './index.css'
 import Contactme from './components/contact/Contactme'
 import Planes from './components/plans/Planes'
-
+import './App.css'
+import Home from './components/Home/Home'
 function App() {
 
   return (
     <div>
+      <Home/>
       <Planes />
       <Contactme />
     </div>
---
+++ frontend/src/components/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './Hero/Hero'
+import Info from './Info/Info'
+import Testimonials from './Testimonials/Testimonials'
+const Home = () => {
+    return (
+        <>
+            <Hero/>
+            <Info/>
+            <Testimonials/>
+        </>
+        
+    );
+}
+
+export default Home;
