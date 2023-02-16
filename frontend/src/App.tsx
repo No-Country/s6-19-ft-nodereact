8cdc202178bdf220648f110ebbf46209cc2e3f93
AlexanderC <alex88_upt@hotmail.com>
2023-02-15 21:55:20 -0500

    register

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,15 @@ import './index.css'
 import './App.css'
 import Home from './components/Home/Home'
 import Navbar from './components/Navbar/Navbar'
+import Register from './components/Register/Register'
 
 function App() {
 
   return (
     <>
-     <Navbar/>
-     <Home/>
+     {/**<Navbar/>**/ }
+     {/**<Home/>**/ }
+     <Register/>
     </>
    
   )
